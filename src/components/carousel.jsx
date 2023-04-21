5e111b57873a093ba3e6268270bc6a06d6023714
ps7355 <ps7355@srmist.edu.in>
2023-04-21 21:27:31 +0530

    Update carousel.jsx

--- src/components/carousel.jsx
+++ src/components/carousel.jsx
@@ -3,7 +3,7 @@ import Carousel  from "nuka-carousel";
 
 function carousel(){
     return <Carousel autoplay={true} autoplayInterval={3000} wrapAround={true}>
-    <img src="src/assets/1 (1).jpg" alt="" />
+    <img src="https://ibb.co/P6BxsVg" alt="" />
     <img src="src/assets/1 (2).jpg" alt="" />
     <img src="src/assets/1 (3).jpg" alt="" />
     <img src="src/assets/1 (4).jpg" alt="" />
