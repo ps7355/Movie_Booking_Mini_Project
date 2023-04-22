d02c71319a300af8cc51806b9c840f8d3e5652b3
ps7355 <ps7355@srmist.edu.in>
2023-04-22 10:00:14 +0530

    image updated

--- src/components/carousel.jsx
+++ src/components/carousel.jsx
@@ -3,12 +3,12 @@ import Carousel  from "nuka-carousel";
 
 function carousel(){
     return <Carousel autoplay={true} autoplayInterval={3000} wrapAround={true}>
-    <img src="https://ibb.co/P6BxsVg" alt="" />
-    <img src="src/assets/1 (2).jpg" alt="" />
-    <img src="src/assets/1 (3).jpg" alt="" />
-    <img src="src/assets/1 (4).jpg" alt="" />
-    <img src="src/assets/1 (5).jpg" alt="" />
-    <img src="src/assets/1 (6).jpg" alt="" />
+    <img src="https://img.ticketnew.com/tn/offer_banner/Rudhran/1920_400.jpg" alt="" />
+    <img src="https://img.ticketnew.com/tn/offer_banner/Thiruvin_kural/1920_400.jpg" alt="" />
+    <img src="https://img.ticketnew.com/tn/offer_banner/Vidyasagar/Website_Banner.jpg" alt="" />
+    <img src="https://img.ticketnew.com/tn/offer_banner/Digit/insurence.jpg" alt="" />
+    <img src="https://cdn3.ticketnew.com/tn/offer_banner/Digit/accidental.jpg" alt="" />
+    <img src="https://img.ticketnew.com/tn/offer_banner/Digit/callsupport.jpg" alt="" />
   </Carousel>
 }
 
