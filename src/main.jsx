6ad908b3561eb096ac5968efca2f131e57c3bef3
ps7355 <ps7355@srmist.edu.in>
2023-09-12 22:41:51 +0530

    1

--- src/main.jsx
+++ src/main.jsx
@@ -10,7 +10,7 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ChakraProvider>
     <BrowserRouter>
-    <BlogSummarizer/>
+    <Navigation/>
     </BrowserRouter>
     </ChakraProvider>
   </React.StrictMode>,
